12ef2039fb8c4cabb0cf29bf7f6759bab525eba8
zhongjun <zhongjun@TAKETOPARMPC>
2024-09-15 19:08:41 +0800

    泰顶WEB服务器
    泰顶WEB服务器

--- TakeTopServer/restore.bat
+++
@@ -1,3 +0,0 @@
-set PGPASSWORD=zxckkllzly
-echo on
-..\pgsetup\postgresql\bin\pg_restore.exe -h 127.0.0.1 -p 5432 -U postgres -w -d taketopdecmpdb -v ..\database\TakeTopAppDBBackup.bak
--- TakeTopServer/restore_.bat
+++
@@ -1,3 +0,0 @@
-set PGPASSWORD=liyisong
-echo on
-pg_restore -h 127.0.0.1 -p 5432 -U postgres -w -d taketopecmpdb -v 20220106.backup
